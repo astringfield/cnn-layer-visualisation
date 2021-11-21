71c3750fe62dbb9f10ba064af7180c756e1d5d5e
SOMNOMED\AStringfield <astringfield@somnomed.com>
2021-11-21 18:02:07 +1100

    feat: add full code from Keras demo source: https://github.com/keras-team/keras-io/blob/5fb698f8c58e1e9a0f9e1f2c75c68e4fbb494f65/guides/writing_a_training_loop_from_scratch.py

--- main.py
+++ main.py
@@ -1,29 +1,58 @@
+"""
+Title: Writing a training loop from scratch
+Author: [fchollet](https://twitter.com/fchollet)
+Date created: 2019/03/01
+Last modified: 2020/04/15
+Description: Complete guide to writing low-level training & evaluation loops.
+"""
+"""
+## Setup
+"""
+
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
-import time
 
+"""
+## Introduction
+Keras provides default training and evaluation loops, `fit()` and `evaluate()`.
+Their usage is covered in the guide
+[Training & evaluation with the built-in methods](/guides/training_with_built_in_methods/).
+If you want to customize the learning algorithm of your model while still leveraging
+the convenience of `fit()`
+(for instance, to train a GAN using `fit()`), you can subclass the `Model` class and
+implement your own `train_step()` method, which
+is called repeatedly during `fit()`. This is covered in the guide
+[Customizing what happens in `fit()`](/guides/customizing_what_happens_in_fit/).
+Now, if you want very low-level control over training & evaluation, you should write
+your own training & evaluation loops from scratch. This is what this guide is about.
+"""
 
-inputs = keras.Input(shape=(784,), name="digits")
-x1 = layers.Dense(64, activation="relu")(inputs)
-x2 = layers.Dense(64, activation="relu")(x1)
-outputs = layers.Dense(10, name="predictions")(x2)
-model = keras.Model(inputs=inputs, outputs=outputs)
+"""
+## Using the `GradientTape`: a first end-to-end example
+Calling a model inside a `GradientTape` scope enables you to retrieve the gradients of
+the trainable weights of the layer with respect to a loss value. Using an optimizer
+instance, you can use these gradients to update these variables (which you can
+retrieve using `model.trainable_weights`).
+Let's consider a simple MNIST model:
+"""
+
+# inputs = keras.Input(shape=(784,), name="digits")
+# x1 = layers.Dense(64, activation="relu")(inputs)
+# x2 = layers.Dense(64, activation="relu")(x1)
+# outputs = layers.Dense(10, name="predictions")(x2)
+# model = keras.Model(inputs=inputs, outputs=outputs)
 
 """
 Let's train it using mini-batch gradient with a custom training loop.
 First, we're going to need an optimizer, a loss function, and a dataset:
 """
 
-# Instantiate an optimizer.
-optimizer = keras.optimizers.SGD(learning_rate=1e-3)
-# Instantiate a loss function.
-loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-# Prepare the metrics.
-train_acc_metric = keras.metrics.SparseCategoricalAccuracy()
-val_acc_metric = keras.metrics.SparseCategoricalAccuracy()
+# # Instantiate an optimizer.
+# optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+# # Instantiate a loss function.
+# loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 # Prepare the training dataset.
 batch_size = 64
@@ -57,6 +86,78 @@ of the model with regard to the loss
 gradients
 """
 
+# epochs = 2
+# for epoch in range(epochs):
+#     print("\nStart of epoch %d" % (epoch,))
+#
+#     # Iterate over the batches of the dataset.
+#     for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
+#
+#         # Open a GradientTape to record the operations run
+#         # during the forward pass, which enables auto-differentiation.
+#         with tf.GradientTape() as tape:
+#
+#             # Run the forward pass of the layer.
+#             # The operations that the layer applies
+#             # to its inputs are going to be recorded
+#             # on the GradientTape.
+#             logits = model(x_batch_train, training=True)  # Logits for this minibatch
+#
+#             # Compute the loss value for this minibatch.
+#             loss_value = loss_fn(y_batch_train, logits)
+#
+#         # Use the gradient tape to automatically retrieve
+#         # the gradients of the trainable variables with respect to the loss.
+#         grads = tape.gradient(loss_value, model.trainable_weights)
+#
+#         # Run one step of gradient descent by updating
+#         # the value of the variables to minimize the loss.
+#         optimizer.apply_gradients(zip(grads, model.trainable_weights))
+#
+#         # Log every 200 batches.
+#         if step % 200 == 0:
+#             print(
+#                 "Training loss (for one batch) at step %d: %.4f"
+#                 % (step, float(loss_value))
+#             )
+#             print("Seen so far: %s samples" % ((step + 1) * batch_size))
+
+"""
+## Low-level handling of metrics
+Let's add metrics monitoring to this basic loop.
+You can readily reuse the built-in metrics (or custom ones you wrote) in such training
+loops written from scratch. Here's the flow:
+- Instantiate the metric at the start of the loop
+- Call `metric.update_state()` after each batch
+- Call `metric.result()` when you need to display the current value of the metric
+- Call `metric.reset_states()` when you need to clear the state of the metric
+(typically at the end of an epoch)
+Let's use this knowledge to compute `SparseCategoricalAccuracy` on validation data at
+the end of each epoch:
+"""
+
+# Get model
+inputs = keras.Input(shape=(784,), name="digits")
+x = layers.Dense(64, activation="relu", name="dense_1")(inputs)
+x = layers.Dense(64, activation="relu", name="dense_2")(x)
+outputs = layers.Dense(10, name="predictions")(x)
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+# Instantiate an optimizer to train the model.
+optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+# Instantiate a loss function.
+loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+# Prepare the metrics.
+train_acc_metric = keras.metrics.SparseCategoricalAccuracy()
+val_acc_metric = keras.metrics.SparseCategoricalAccuracy()
+
+"""
+Here's our training & evaluation loop:
+"""
+
+import time
+
 epochs = 2
 for epoch in range(epochs):
     print("\nStart of epoch %d" % (epoch,))
@@ -64,26 +165,10 @@ for epoch in range(epochs):
 
     # Iterate over the batches of the dataset.
     for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
-
-        # Open a GradientTape to record the operations run
-        # during the forward pass, which enables auto-differentiation.
         with tf.GradientTape() as tape:
-
-            # Run the forward pass of the layer.
-            # The operations that the layer applies
-            # to its inputs are going to be recorded
-            # on the GradientTape.
-            logits = model(x_batch_train, training=True)  # Logits for this minibatch
-
-            # Compute the loss value for this minibatch.
+            logits = model(x_batch_train, training=True)
             loss_value = loss_fn(y_batch_train, logits)
-
-        # Use the gradient tape to automatically retrieve
-        # the gradients of the trainable variables with respect to the loss.
         grads = tape.gradient(loss_value, model.trainable_weights)
-
-        # Run one step of gradient descent by updating
-        # the value of the variables to minimize the loss.
         optimizer.apply_gradients(zip(grads, model.trainable_weights))
 
         # Update training metric.
@@ -95,7 +180,7 @@ for epoch in range(epochs):
                 "Training loss (for one batch) at step %d: %.4f"
                 % (step, float(loss_value))
             )
-            print("Seen so far: %d samples" % ((step + 1) * 64))
+            print("Seen so far: %d samples" % ((step + 1) * batch_size))
 
     # Display metrics at the end of each epoch.
     train_acc = train_acc_metric.result()
